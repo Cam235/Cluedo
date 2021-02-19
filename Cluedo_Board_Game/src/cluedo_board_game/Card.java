3a39ef6d55c4ef49401e8187aa72141e1d44ad9d
Charlie <cw523@sussex.ac.uk>
2021-02-19 15:35:06 +0000

    Created Card Class
    created card class and separate class for CardType enum so that it can be used properly by other classes

---
+++ Cluedo_Board_Game/src/cluedo_board_game/Card.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluedo_board_game;
+
+/**
+ *
+ * @author 
+ */
+public class Card {
+    
+    private CardType type;
+    private String name; 
+    
+    public Card(CardType cardType, String cardName){
+        type = cardType;
+        name = cardName;
+    }
+    
+    public String getName(){
+        return name;
+    }
+    
+    public void setName(String newName){
+        name = newName;
+    }
+    
+    public String getType(){
+        return type.toString();
+    }
+    
+    public void setType(CardType newType){
+        type = newType;
+    }
+
+}
---
+++ Cluedo_Board_Game/src/cluedo_board_game/CardType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cluedo_board_game;
+
+/**
+ *
+ * @author 
+ */
+enum CardType {
+        Person,
+        Weapon,
+        Room
+}
