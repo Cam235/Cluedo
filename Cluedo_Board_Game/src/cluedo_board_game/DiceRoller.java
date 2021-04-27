a02b9c74dd91f977cc94e562bbc0e08dde743959
Anil <az226@sussex.ac.uk>
2021-04-27 20:18:01 +0100

    Diceroller javadoc created

--- Cluedo_Board_Game/src/cluedo_board_game/DiceRoller.java
+++ Cluedo_Board_Game/src/cluedo_board_game/DiceRoller.java
@@ -25,14 +25,15 @@ import javafx.scene.paint.Color;
  *
  * @author Anilz
  */
-public class DiceRoller{
-    private boolean diceRolled;
+public class DiceRoller {
+
+    private boolean diceRolled; //returns whether dice is rolled or not
+
     //Initial Dice Values are given 1
     private int diceNumber = 1;
     private int diceNumber2 = 1;
-//-----------GUI stuff---------//
     //Button to throw dice
-    Button rollButton;
+    private Button rollButton;
     //Initial 1st Dice View
     Image diceImage = new Image("/Die/Dice" + diceNumber + ".png", 75, 75, false, false);
     ImageView diceImageView = new ImageView(diceImage);
@@ -41,16 +42,14 @@ public class DiceRoller{
     ImageView diceImageView2 = new ImageView(diceImage2);
 
     /**
-     * Creates diceroller content 2 Dices and a roll button is added,with their
-     * GUI
+     * Creates DiceRoller content including 2 Dices and a roll button GUI
      *
-     * @return
+     * @return diceRollerBox
      */
-    
     public VBox createContent() {
         rollButton = new Button();
         rollButton.setText("Roll the Dice");
-        rollButton.setPrefSize(150,50);
+        rollButton.setPrefSize(150, 50);
         rollButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -60,49 +59,47 @@ public class DiceRoller{
                 //Gets Value of First And Second Dice
                 diceNumber = randomDice.nextInt(6) + 1;
                 diceNumber2 = randomDice.nextInt(6) + 1;
-                //Sets Image For First Dice
+                //Sets Image For First Dice Image and View
                 diceImage = new Image("/Die/Dice" + diceNumber + ".png", 75, 75, false, false);
                 diceImageView.setImage(diceImage);
-                //Sets Second Dice
+                //Sets Second Dice Image and View
                 diceImage2 = new Image("/Die/Dice" + diceNumber2 + ".png", 75, 75, false, false);
                 diceImageView2.setImage(diceImage2);
                 //Disables button after pressed
                 rollButton.setDisable(true);
             }
         });
-        Background bg = new Background(new BackgroundFill(Color.DARKGRAY, CornerRadii.EMPTY, Insets.EMPTY));
-        
+
         //Displays dices horizontally
         HBox dicesHbox = new HBox();
         dicesHbox.getChildren().addAll(diceImageView, diceImageView2);
-        dicesHbox.setBackground(bg);
-        
         //Displays roll button above dices
         VBox diceRollerBox = new VBox();
         diceRollerBox.getChildren().addAll(rollButton, dicesHbox);
-        //SetsAlignments
+        //Sets Alignments
         dicesHbox.setAlignment(Pos.CENTER);
         diceRollerBox.setAlignment(Pos.CENTER);
-       
+        //Sets background of diceRollerBox
+        Background bg = new Background(new BackgroundFill(Color.DARKGRAY, CornerRadii.EMPTY, Insets.EMPTY));
         diceRollerBox.setBackground(bg);
+
         return diceRollerBox;
     }
 
     /**
+     * Returns true if dice is rolled, and false if dice has not rolled yet
      *
-     * @return true if dice is rolled, and false if dice has not rolled yet
+     * @return diceRolled
      */
-    
     public boolean isDiceRolled() {
         return diceRolled;
     }
 
     /**
-     * sets whether or not dice has rolled
+     * Sets whether or not dice is rolled
      *
      * @param diceRolled
      */
-    
     public void setDiceRolled(boolean diceRolled) {
         this.diceRolled = diceRolled;
     }
@@ -110,20 +107,22 @@ public class DiceRoller{
     /**
      * Enables the diceRoller Button to roll the dice
      */
-    
     public void enableDiceRollerButton() {
         rollButton.setDisable(false);
     }
 
     /**
-     * @return total Dice value from
+     * @return total Dice value 
      */
-    
     public int getDiceTotal() {
         return diceNumber + diceNumber2;
     }
-    
-    public Button getRollButton(){
+
+    /**
+     * Gets the roll button
+     * @return rollButton
+     */
+    public Button getRollButton() {
         return rollButton;
     }
 
