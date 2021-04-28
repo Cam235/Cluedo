fcfecea4718376c4d841b9984988fe4841850785
Anil <az226@sussex.ac.uk>
2021-04-28 15:52:47 +0100

    clean

--- Cluedo_Board_Game/src/cluedo_board_game/Agent.java
+++ Cluedo_Board_Game/src/cluedo_board_game/Agent.java
@@ -8,8 +8,6 @@ package cluedo_board_game;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
